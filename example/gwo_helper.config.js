fff698cbf8690124e45ce75c15702c0f1e860655
Juan Ignacio Donoso <jidonoso@gmail.com>
2011-03-15 20:53:09 -0300

    Fixed bug in the config definition

--- example/gwo_helper.config.js
+++ example/gwo_helper.config.js
@@ -1,28 +1,25 @@
-GWO_helper = {
-    
-    /**
-     * Google Web Optimizer account UA-XXXXXXX-X
-     */
-    account: "UA-8845687-3",
-    
-    /**
-     * When true a summary of the experiment will be showed in the consol
-     * Also the tranking test and goals are omitted
-     */
-    debug: true, 
-    
-    /**
-     * Definition of the experiments
-     */
-    experiments: {
-        signup: {
-            id: "1635239698", /* GWO experiment id */
-            sections: [{name: 'header'}, {name: 'button'}]  /* Section Names */
-        },
-        viewpicture: {   
-            id: "3478664147",
-            sections: [{name: 'picture'},{name: 'caption'}]
-        }
-    }       
-};
+/**
+* Google Web Optimizer account UA-XXXXXXX-X
+*/
+GWO_helper.account: "UA-8845687-3",
+
+/**
+* When true a summary of the experiment will be showed in the consol
+* Also the tranking test and goals are omitted
+*/
+GWO_helper.debug: true, 
+
+/**
+* Definition of the experiments
+*/
+GWO_helper.experiments: {
+	signup: {
+	    id: "1635239698", /* GWO experiment id */
+	    sections: [{name: 'header'}, {name: 'button'}]  /* Section Names */
+	},
+	viewpicture: {   
+	    id: "3478664147",
+	    sections: [{name: 'picture'},{name: 'caption'}]
+	}
+}    
 
