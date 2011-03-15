aaae6d62ca1bc22a41ab3b4ccb8e46dac456a582
Juan Ignacio Donoso <jidonoso@gmail.com>
2011-03-15 20:58:40 -0300

    Fixed bug in the config definition

--- gwo_helper.config.js
+++ gwo_helper.config.js
@@ -1,18 +1,18 @@
 /**
 * Google Web Optimizer account UA-XXXXXXX-X
 */
-GWO_helper.account: "UA-XXXXXXX-X";
+GWO_helper.account = "UA-XXXXXXX-X";
     
 /**
 * When true a summary of the experiment will be showed in the consol
 * Also the tranking test and goals are omitted
 */
-GWO_helper.debug: true;
+GWO_helper.debug = true;
 
 /**
 * Definition of the experiments
 */
-GWO_helper.experiments: {
+GWO_helper.experiments = {
 	example1: {
 	    id: "0000000000", /* GWO experiment id */
 	    sections: [{name: 'section1'}, {name: 'section2'}]  /* Section Names */
