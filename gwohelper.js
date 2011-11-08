440b1b14132d8f589db75684b44aedd19339298b
Juan Ignacio Donoso <jidonoso@gmail.com>
2011-11-08 01:06:15 -0300

    change simplify the code

--- gwohelper.js
+++ gwohelper.js
@@ -1,19 +1,20 @@
 GWO_helper = {
      /**
      * Initialize all the control scripts defined in the experiment object
-     * @callback             function   callback to be executed at the end of the last iteration
+     * @param   _experiments  object      initialization object contains account id, debug flag and experiments ids
+     * @param   callback      function    callback to be executed at the end of the last iteration
      */
-    init: function(callback){
+    init: function(_experiments, callback){
         
-       // Create an array with the keys for each experiment
+       /* Create an array with the keys for each experiment
        var expArray = [];
        for(var experiment in this.experiments) {
            expArray.push(experiment);
-       }
+       }*/
                
        // Load control scripts for each experiments 
        // Load the tracking script when finish
-       this._loadControls(expArray, 0, callback);     
+       this._loadControls(_experiments, 0, callback);     
        
     },
     
@@ -23,15 +24,15 @@ GWO_helper = {
     
     /**
      * Load the GOOGLE CONTROL SCRIPTS recurively
-     * @_experiments         array      array with the names of the experiments defined in GWO_helper.experiments
-     * @idx                  int        index to load
-     * @callback             function   callback to be executed at the end of the last iteration
+     * @param   _experiments    array      array with the names of the experiments defined in GWO_helper.experiments
+     * @param    idx            int        index to load
+     * @param   callback        function   callback to be executed at the end of the last iteration
      */
-    _loadControls: function(_expNames, idx, callback){
+    _loadControls: function(_experiments, idx, callback){
         
         // GOOGLE CONTROL SCRIPT 
         function utmx_section(){}function utmx(){}
-        (function(){var k=GWO_helper.experiments[_expNames[idx]].id,d=document,l=d.location,c=d.cookie;function f(n){
+        (function(){var k=_experiments[idx].id,d=document,l=d.location,c=d.cookie;function f(n){
         if(c){var i=c.indexOf(n+'=');if(i>-1){var j=c.indexOf(';',i);return escape(c.substring(i+n.
         length+1,j<0?c.length:j))}}}var x=f('__utmx'),xx=f('__utmxx'),h=l.hash;
         var he = d.getElementsByTagName("head")[0];
@@ -53,15 +54,19 @@ GWO_helper = {
 
         var loadCallback = function(){
             // Get the variations
-            GWO_helper._resolveVariations(_expNames[idx]);
-
+            GWO_helper._resolveVariations(_experiments[idx]);
+            
             // Debug in the console
             if(GWO_helper.debug)
-                GWO_helper._showlog(_expNames[idx]);
+                GWO_helper._showlog(_experiments[idx].name);
+                
+            // Track this experiment if the flag is true
+            if(_experiments[idx].track)
+                GWO_helper.experiments[_experiments[idx].name].start();
 
             // Recursively load controls for each experiment
-            if(idx + 1 < _expNames.length)
-                GWO_helper._loadControls(_expNames, idx + 1, callback);
+            if(idx + 1 < _experiments.length)
+                GWO_helper._loadControls(_experiments, idx + 1, callback);
             else{
                 // Load the tracking script
                 GWO_helper._loadTracking()
@@ -95,21 +100,21 @@ GWO_helper = {
     /**
      * Load the GOOGLE TRACKING SCRIPT
      */
-    _showlog: function(_exp, _type){
+    _showlog: function(_expnmame, _type){
         
         // DEBUG
         if(_type == undefined){
             console.log("******************************************************************");
-            console.log("LOADED EXPERIMENT " + _exp, this.experiments[_exp]);
-            console.log("TEST COMBINATION", this.experiments[_exp].combination);
-            for(var i = 0; i < this.experiments[_exp].sections.length; i++){
-                console.log("TEST VARIATIONS", this.experiments[_exp].sections[i].name + " - " + this.experiments[_exp].sections[i].variation);
+            console.log("LOADED EXPERIMENT " + _expnmame, this.experiments[_expnmame]);
+            console.log("TEST COMBINATION", this.experiments[_expnmame].combination);
+            for(var i = 0; i < this.experiments[_expnmame].sections.length; i++){
+                console.log("TEST VARIATIONS", this.experiments[_expnmame].sections[i].name + " - " + this.experiments[_expnmame].sections[i].variation);
             }
             console.log("******************************************************************");
         }
         else {
             console.log("******************************************************************");
-            console.log("TRACKING EXPERIMENT", _exp, _type);
+            console.log("TRACKING EXPERIMENT", _expnmame, _type);
             console.log("******************************************************************");
         }
         
@@ -119,43 +124,51 @@ GWO_helper = {
      * Get the variations for each section of the experiment. Also get the combination
      */
     _resolveVariations: function(_exp){
+    
+        var name = _exp.name;
+        if(!this.experiments) this.experiments = {}; 
         
         // Get the forced variations
-        //this._getForcedVariations();
-
-        // Request the variations with the utmx function
-        for(var k = 0; k < this.experiments[_exp].sections.length; k++){
-            this.experiments[_exp].sections[k].variation = utmx("variation_number", this.experiments[_exp].sections[k].name);
-        }	
+        //this._getForcedVariations();       
+        
+        // Set the id
+        this.experiments[name] = {id: _exp.id};
         
         // Set the combination for this experiment
-        this.experiments[_exp].combination = utmx("combination");
+        this.experiments[name].combination = utmx("combination");
         
         // Set th utmx function to the experiment
-        this.experiments[_exp].utmx = utmx;
+        this.experiments[name].utmx = utmx;
         
         // Set the tracking functions for the experiments
-        this.experiments[_exp].start = function(){
+        this.experiments[name].start = function(){
             if(!GWO_helper.debug){
-                _gaq.push(['gwo._trackPageview', '/' + GWO_helper.experiments[_exp].id + '/test']);
+                _gaq.push(['gwo._trackPageview', '/' + GWO_helper.experiments[name].id + '/test']);
             }
             else{
-                GWO_helper._showlog(_exp, "test");
+                GWO_helper._showlog(name, "test");
             }
         };
-        this.experiments[_exp].goal = function(){
+        this.experiments[name].goal = function(){
             if(!GWO_helper.debug){
-                _gaq.push(['gwo._trackPageview', '/' + GWO_helper.experiments[_exp].id + '/goal']);
+                _gaq.push(['gwo._trackPageview', '/' + GWO_helper.experiments[name].id + '/goal']);
             }
             else{
-                GWO_helper._showlog(_exp, "goal");
+                GWO_helper._showlog(name, "goal");
             }
         };
         
-        // ADD CLASS TO THE BODY  state-test(experiment)_(section)-(variation)
-        var b = document.getElementsByTagName("body")[0];
-        for(var l = 0; l < this.experiments[_exp].sections.length; l++){
-            var testState = "state-test-" + (_exp) + "-" + l + "-" + this.experiments[_exp].sections[l].variation;
+        // Prepare section container
+        this.experiments[name].sections = []; 
+        
+        // Resolve sections
+        for(var section in utmx_global_vd) {
+            // Save the variations
+            this.experiments[name].sections.push({name: section, variation: utmx_global_vd[section].index});
+            
+            // ADD CLASS TO THE BODY  gwo-(experiment)_(section)-(variation)
+            var b = document.getElementsByTagName("body")[0];
+            var testState = "gwo-" + name + "-" + section + "-" + utmx_global_vd[section].index;
             
             if (!b.className.match(new RegExp('(\\s|^)'+testState+'(\\s|$)'))) 
                 b.className += " "+testState;
